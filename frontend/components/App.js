2fe1b1ef7727f53dab53caa44c3494cd2d3c5eda
Troy Delacruz <troy8076@yahoo.com>
2023-04-21 02:45:52 -0400

    Envisioning/working on layout

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,9 +3,24 @@ import React from 'react'
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        Todo App
-      </div>
+     <>
+     
+      <h1>Todos</h1>
+
+      <ul>
+        <li>Walk the dog</li>
+        <li>Take out trash</li>
+        <li>Workout</li>
+      </ul>
+
+      <form>
+        <input />
+        <button>Add</button>
+      </form>
+
+      <button>Clear</button>
+
+     </>
     )
   }
 }
