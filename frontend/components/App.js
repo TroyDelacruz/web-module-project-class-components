4e47554dc4d7507460f9f77b4aea098faf622664
Troy Delacruz <troy8076@yahoo.com>
2023-04-21 03:24:49 -0400

    Working on todolist

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React from 'react';
+import TodoList from './TodoList';
+import Form from './Form';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false)
+      })
+    })
+  }
+
   render() {
+    const { todos } = this.state;
+
     return (
      <>
      
       <h1>Todos</h1>
 
-      <ul>
-        <li>Walk the dog</li>
-        <li>Take out trash</li>
-        <li>Workout</li>
-      </ul>
-
-      <form>
-        <input />
-        <button>Add</button>
-      </form>
-
-      <button>Clear</button>
+      <TodoList todos={todos} />
+      <Form />
+      <button onClick={this.handleClear}>Clear</button>
 
      </>
     )
